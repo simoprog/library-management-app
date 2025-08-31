df2bb783cbd198ad391e9faf3f169e75504f1265
Mohamed El Arfaoui <simoelarfaoui5@gmail.com>
2025-08-31 17:28:32 +0100

    finish setup

---
+++ Domain/Events/IDomainEvent.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace library_management_api.Domain.Events;
+
+public interface IDomainEvent : INotification
+{
+    Guid Id { get; }
+    DateTime OccurredOn { get; }
+    
+}
