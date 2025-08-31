3aa43bddb6901d05ed067873d4e82d1e3f9761df
Mohamed El Arfaoui <simoelarfaoui5@gmail.com>
2025-08-31 21:37:37 +0100

    Add services

---
+++ Domain/Services/PlacingOnHoldPolicy.cs
@@ -0,0 +1,34 @@
+﻿using library_management_api.Domain.Entities;
+
+namespace library_management_api.Domain.Services;
+
+public class PlacingOnHoldPolicy
+{
+    public bool CanPlaceOnHold(Book book, Patron patron)
+    {
+        if (!patron.IsActive)
+            return false;
+            
+        if (patron.HasOutstandingFees())
+            return false;
+            
+        if (book.IsRestrictedAccess && !patron.CanHoldRestrictedBooks())
+            return false;
+            
+        return true;
+    }
+    
+    public string GetRejectionReason(Book book, Patron patron)
+    {
+        if (!patron.IsActive)
+            return "Patron account is inactive";
+            
+        if (patron.HasOutstandingFees())
+            return "Patron has outstanding fees";
+            
+        if (book.IsRestrictedAccess && !patron.CanHoldRestrictedBooks())
+            return "Patron cannot access restricted books";
+            
+        return "Hold request is valid";
+    }
+}
